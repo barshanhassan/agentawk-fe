fe174f3e7cfe34324187fbd6df5819a49819ac21
Daniel Sarfraz <daniel.sarfraz@ezauq.com>
2025-11-14 16:46:42 +0500

    Update PreviewV2.tsx

--- client/src/components/PreviewV2.tsx
+++ client/src/components/PreviewV2.tsx
@@ -285,7 +285,7 @@ const PreviewV2: React.FC<PreviewV2Props> = ({
 
       {mode === 'chat' && (
         <>
-          <div className={`z-0 w-full max-h-[72px] h-full bg-white -mt-px flex items-center justify-between px-[16px] ${!showTopBar ? 'rounded-t-[14px]' : ''}`} style={{ boxShadow: 'inset 0 -3px 0 0 #e6e6e6' }}>
+          <div className={`w-full max-h-[72px] h-full bg-white -mt-px flex items-center justify-between px-[16px] ${!showTopBar ? 'rounded-t-[14px]' : ''}`} style={{ boxShadow: 'inset 0 -3px 0 0 #e6e6e6' }}>
             <div className="flex items-center">
               <ArrowLeft size={24}/>
               <img src={profilePfpUrl} className="ml-[10px] mr-[9px] w-[40px] h-[40px] bg-gray-300 rounded-full object-cover" alt="Profile Picture" />
@@ -478,18 +478,19 @@ const PreviewV2: React.FC<PreviewV2Props> = ({
 
       {mode === 'profile' && (
         <>
+          <div className='relative flex justify-center'>
+            <img src={profilePfpUrl} className="absolute w-[130px] top-[24px] h-[130px] rounded-full z-10 object-cover" alt="Profile" />
+          </div>
           <div className={`relative z-0 w-full max-h-[72px] h-full bg-white -mt-px flex items-center justify-between px-[16px] ${!showTopBar ? 'rounded-t-[14px]' : ''}`}>
             <ArrowLeft size={24}/>
             <MoreVertical size={24} />
           </div>
           <div className={`pt-[90px] pb-[30px] w-full h-full bg-white -mt-px px-[17px] overflow-y-auto overflow-x-hidden flex flex-col scrollbar-hide ${!showBottomBar ? 'rounded-b-[14px]' : ''}`}>
-            <div className='relative flex justify-center'>
-              <img src={profilePfpUrl} className="absolute z-10 w-[130px] top-[-154px] h-[130px] rounded-full mb-[8px] object-cover" alt="Profile" />
-            </div>
+            <div className="h-[70px]" />
             {/* Profile Picture, Name, ~Phone Number, Share Button */}
             <div className="flex flex-col items-center text-center mb-[2px]">
-              <h2 className="text-[24px] font-semibold max-w-[300px] leading-7 break-all">{profileName}</h2>
-              {profilePhoneNumber && <p className="mt-[4px] text-[19px] text-gray-500 max-w-[300px] break-all">{profilePhoneNumber}</p>}
+              <h2 className="text-[24px] font-semibold max-w-[300px] leading-7">{profileName}</h2>
+              {profilePhoneNumber && <p className="mt-[4px] text-[19px] text-gray-500 max-w-[300px]">{profilePhoneNumber}</p>}
               <div className="mt-[24px] py-[6px] px-[44px] flex flex-col items-center justify-center border rounded-[10px] shadow-xs border-[var(--button-outline)]">
                 <Forward size={24} color="#36AD60" />
                 <h3>Share</h3>
@@ -501,11 +502,11 @@ const PreviewV2: React.FC<PreviewV2Props> = ({
               <>
                 <div className='my-[22px] border-t-[2px] border-[#e7e7e7ff] w-[calc(100% + 16px)] ml-[16px]'/>
                 <div>
-                  {profileDescription && <p className="text-gray-700 mb-2 break-all">{profileDescription}</p>}
+                  {profileDescription && <p className="text-gray-700 mb-2">{profileDescription}</p>}
                   {profileCategory && <p className="text-gray-500 mb-2">{profileCategory}</p>}
-                  {profileAddress && <p className="text-gray-500 mb-2 break-all">{profileAddress}</p>}
-                  {profileEmail && <p className="text-blue-500 mb-2 break-all">{profileEmail}</p>}
-                  {profileWebsite && <p className="text-blue-500 break-all">{profileWebsite}</p>}
+                  {profileAddress && <p className="text-gray-500 mb-2">{profileAddress}</p>}
+                  {profileEmail && <p className="text-blue-500 mb-2">{profileEmail}</p>}
+                  {profileWebsite && <p className="text-blue-500">{profileWebsite}</p>}
                 </div>
               </>
             )}
@@ -518,13 +519,13 @@ const PreviewV2: React.FC<PreviewV2Props> = ({
                   {profileAbout && (
                     <div>
                       <h3 className="font-semibold mb-1">About</h3>
-                      <p className="text-gray-700 break-all">{profileAbout}</p>
+                      <p className="text-gray-700">{profileAbout}</p>
                     </div>
                   )}
                   {profilePhoneNumber && (
                     <div className="mt-2">
                       <h3 className="font-semibold mb-1">Phone Number</h3>
-                      <p className="text-gray-700 break-all">{profilePhoneNumber}</p>
+                      <p className="text-gray-700">{profilePhoneNumber}</p>
                     </div>
                   )}
                 </div>
