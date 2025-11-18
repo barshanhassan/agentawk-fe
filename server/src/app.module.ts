29591d6606cab6555e5603f531dbc47a5ef65830
Daniel Sarfraz <daniel.sarfraz@ezauq.com>
2025-11-18 17:27:41 +0500

    Init basic server modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { SessionModule } from './session/session.module';
+import { DatabaseModule } from './database/database.module';
+import { CacheModule } from './cache/cache.module';
+import { UserModule } from './user/user.module';
+import { WorkspaceModule } from './workspace/workspace.module';
+import { RoleModule } from './role/role.module';
+import { TennantModule } from './tennant/tennant.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, WorkspaceModule, TennantModule, RoleModule, UserModule, CacheModule, DatabaseModule, SessionModule],
   controllers: [AppController],
   providers: [AppService],
 })
